1062bfbe34156b28aabf193d3c09fd1422e071d0
sandeep-6 <sandeep.saliganti@x-verity.com>
2025-07-12 01:23:34 +0530

    pushing the apps minor ui changes

--- frontend/src/pages/Apps_module/AppManagement.tsx
+++ frontend/src/pages/Apps_module/AppManagement.tsx
@@ -276,10 +276,16 @@ const AppManagement: React.FC = () => {
         {stepLabels.map((step: { label: string; sub: string }, idx: number) => (
           <div
             key={step.label}
-            className={`p-5 rounded-lg transition-all dark:border-dark-border bg-white dark:bg-primary-dark ${idx === currentStep - 1 ? 'shadow-[0px_5px_5px_-2px_rgba(220,228,239,1)]' : ''}`}
+            className={`p-5 rounded-lg transition-all dark:border-dark-border bg-white dark:bg-primary-dark flex justify-between items-center gap-4  ${idx === currentStep - 1 ? 'shadow-[0px_5px_5px_-2px_rgba(220,228,239,1)]' : ''}`}
           >
+            <div className='flex items-center justify-center min-w-8 h-8 px-2 rounded-full bg-primary text-white text-sm font-semibold'>
+              {idx + 1}
+            </div>
+            <div className='flex flex-col gap-2'>
             <h2 className={`font-semibold  ${idx === currentStep - 1 ? 'text-primary dark:text-white' : 'text-main dark:text-white'}`}>{step.label}</h2>
             <h3 className="text-md text-gray-500 dark:text-gray-400">{step.sub}</h3>
+            </div>
+          
           </div>
         ))}
       </div>
